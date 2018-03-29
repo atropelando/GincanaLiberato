9ec44529db2c3e2bd176eb51003cddb5949741ad
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2018-03-28 21:02:58 -0300

    Adicionar Trio Eletro e videos/originais

--- data/Songs.js
+++ data/Songs.js
@@ -25,6 +25,7 @@ A, aha, e a TRÔ vai comandar! [2x]`
       },
       {
         title: "aTRÔpelando Canta",
+        video: "https://www.youtube.com/watch?v=CKJUoC8QGnc",
         lyrics:
 `aTRÔpelando canta!
 A Liberato encanta!
@@ -41,6 +42,7 @@ E só da TRÔ!
       {
         title: "Guerreiros Verdes",
         original: "Amigo (Roberto Carlos)",
+        video: "https://www.youtube.com/watch?v=yppVGigp2qI",
         lyrics: 
 `Tropa de guerreiros verdes que lutam por glória
 Garra e sabedoria escrevem tua história
@@ -54,6 +56,7 @@ Tu pintou o inferno de verde, é atropelando, guerreira imortal.`
       {
         title: "Meter Neles",
         original: "Can't Take My Eyes Off Of You (Frankie Valli)",
+        video: "https://www.youtube.com/watch?v=l-iFfDZUtBE",
         lyrics:
 `aTRÔpelando
 Meter neles vamos, aTRÔpelando
@@ -94,6 +97,7 @@ Nós somos aTRÔpelando até morrer!`
       {
         title: "Avante minha Trô",
         original: "Solo Le Pido A Dios (Mercedes Sosa)",
+        video: "https://www.youtube.com/watch?v=BACr0RxhpbM",
         lyrics:
 `Avante minha Trô
 Lutaremos juntos outra vez
@@ -108,6 +112,7 @@ Porque Trô tu és minha vida eu te levo no peito até morrer!`
       },
       {
         title: "Saiu do almox",
+        video: "https://www.youtube.com/watch?v=CKJUoC8QGnc",
         lyrics:
 `Saiu do almox para nos orientar
 Uhu, é Vanderlei, que chegou pra atropelar (x2)
@@ -130,6 +135,7 @@ Este ano nós queremos festejar`
       {
         title: "Mamonas",
         original: "Pelados em Santos (Mamonas Assassinas)",
+        video: "https://www.youtube.com/watch?v=59vmB_W_3f0",
         lyrics:
 `TRÔOO, estaremos contigo 
 
@@ -152,6 +158,29 @@ E dalhe dalhe TRÔ
 E dalhe dalhe TRÔ
 E dalhe dalhe TRÔ
 É a TRÔ do meu coração`
+      },
+      {
+        title: "Amor descontrolado",
+        video: "https://www.youtube.com/watch?v=BWgDJ_WAwiw",
+        lyrics:
+`sse amor descontrolado
+Nunca vou deixar de lado
+Vamos juntos minha Trô
+Eu te sigo aonde for
+
+Sempre honrando a camiseta
+Porque Trô tu és copeira
+Hoje junto da bandeira
+Nós temos que festejar
+
+Já tem muito tempo
+Que eu venho a te apoiar
+Não importa o que digam
+Junto vou estar
+
+Por isso que eu te falo
+Que a Trô copará
+O Show nunca acabará (2x)`
       },
       {
         title: "Por que será?",
@@ -190,6 +219,7 @@ aTRÔpelaaandooo`
       },
       {
         title: "Inferno Verde voltou",
+        video: "https://www.youtube.com/watch?v=qFDutfzIH9c",
         lyrics:
 `ÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔÔ
 
@@ -202,6 +232,7 @@ O inferno verde voltou`
       {
         title: "A hora é agora",
         original: "We Will Rock You (Queen)",
+        video: "https://www.youtube.com/watch?v=t6g3xstXw2o",
         lyrics:
 `A hora é agora vamos lá vamos em frente
 Mostrar por que o verde é tão diferente
@@ -385,6 +416,7 @@ A MECA vai vencer!`
       },
       {
         title: "Pense em Mim",
+        original: "Pense em mim (Leandro e Leonardo)",
         lyrics: 
 `Em vez de você ficar pensando nele
 Em vez de você viver chorando por ele
@@ -428,12 +460,247 @@ Desde o início eu lutei por ti.`
     },
     data: [
       {
-        title: "Placeholder",
+        title: "E ninguém cala",
+        video: "http://www.youtube.com/watch?v=rM495SbDlZw&feature=related",
         lyrics:
-`Hoje
-eu acordei
-me sentindo
-um chuchu`
+`E ninguém cala
+Esse nosso amor
+E é por isso que eu canto assim
+Trio EletroôÔ (x2)
+Ôoôoôôô
+Ôoôoôôô
+Ôoô ôô
+Ôoô ôô
+Trio Eletrôôoo`
+      },
+      {
+        title: "Somos azules",
+        video: "http://www.youtube.com/watch?v=rM495SbDlZw&feature=related",
+        lyrics:
+`Somos, somos azules
+E nesta tarde
+Te quero ver ganhar.
+Te quero!
+Te quero!
+Te quero!`
+      },
+      {
+        title: "Pingos de amor",
+        video: "http://www.youtube.com/watch?gl=BR&hl=pt&v=Ifj4tuqMXps",
+        lyrics:
+`A vida passa eu telefono e você já não me atende mais (TRIO! TRIO!)
+Será que já não temos tempoe e nem coragem de dialogar... (TRIO! TRIO!)
+Ainda ontem pela praia alguma coisa me lembrou você! (TRIO! TRIO!)
+E venho a noite namorados se encontrando e eu estava só... (TRIO! TRIO!)
+Vamos ser, outra vez nós dois...
+Vai chover,pingos de amor Ôôôôôôôô !
+Laialaialaialaialaialaialaialaiaaa (TRIO! TRIO!)
+Laialaialaialaialaialaialaialaiaaa (TRIO! TRIO!)`
+      },
+      {
+        title: "Me leva que eu vou",
+        video: "http://www.youtube.com/watch?v=t3U6dIewVb8",
+        lyrics:
+`ôôôÔ me leva que eu vou,
+Sonho meu
+Atrás do Trio Eletro só não vai quem já morreu`
+      },
+      {
+        title: "Galera Bob Marley",
+        video: "http://www.youtube.com/watch?v=CosRqbAlaJM",
+        lyrics:
+`Por favor, Por favor, Por favor se espalhem
+Que lá vem chegando a galera Bob Marley`
+      },
+      {
+        title: "Tema Da Vitória",
+        video: "http://www.youtube.com/watch?v=xlBHt5zz5FE",
+        lyrics:
+`Eu nunca me esquecerei
+Do hexa que ganhei
+Oh Eeeleeetroooo
+Sooou Eletro de coraçãão
+Com orgulho e paixão
+Dale Eeleeetrooo
+Ooo dale dalê daleoooo..
+dale dalê daleoooo.........
+Dalee Eeeleetroo........... (x2)`
+      },
+      {
+        title: "Trio Eletro eu sou",
+        video: "http://br.youtube.com/watch?v=TfAtFlZ-Bqg",
+        lyrics:
+`Dale dale dale dale dale dale daleooooo
+dale daleooooo
+Trio Eletro eu soooôôou`
+      },
+      {
+        title: "Dale dale Eletro",
+        video: "http://br.youtube.com/watch?v=9sYGKIdP01k",
+        lyrics:
+`Dale dale dale dale dale dale dale Eletrooooo
+dale dale dale dale dale dale dale oooooooo
+Dale dale dale dale dale dale dale Eletrooooo
+dale dale dale dale dale dale dale oooo`
+      },
+      {
+        title: "Deixa passar",
+        video: "http://www.youtube.com/watch?v=7nL1seIP-kk",
+        lyrics:
+`Deixa passar
+Deixa passar
+Tá chegando o Trio Eletro
+E o show vai começar.`
+      },
+      {
+        title: "Somos Eletro",
+        video: "http://www.youtube.com/watch?v=eS7xNMHvo-Y",
+        lyrics:
+`Senhores somos eletro e isso não muda
+Nós vamos sair campeões faça sol ou chuva
+Com raça e com coragem
+macacos vamos copar
+Depois traz uma gelada pra festejar
+Dálhe Dálhe ooo
+Dálhe Dálhe ooo
+Dálhe Dálhe ooo
+Dálhe Dálhe ooo.`
+      },
+      {
+        title: "Brutal Terror",
+        video: "http://www.youtube.com/watch?v=n0vB0feYPfE",
+        lyrics:
+`Hoje temos que vencer!
+Mais um estrela eu quero ver
+Hoje vamos festejar
+Trio eletro vamos ganhar
+Eletro tu és a paixão
+Que eu levo no coração!
+Eu sempre vou te apoiar,
+Para te ver ganhar!
+Ô Ô Ô!
+O TRIO ELETRO voltou!
+Ô Ô Ô!
+De volta o BRUTAL TERROR!`
+      },
+      {
+        title: "Passos à vitoria",
+        video: "http://www.youtube.com/watch?v=LMxOWtYvUGw",
+        lyrics:
+`E vamo Eletro só te peço mais essa gincana
+É com amor que eu canto, grito pra te ver ganhar
+Sou Trio Eletro e nada muda esse sentimento
+Porque é nas más que eu demonstro que te amo igual
+E vamo Eletro nao podemos perder
+E vamo Eletro que nós vamos ganhar
+Daria a vida por essa gincana, uma taça a mais`
+      },
+      {
+        title: "Vamo vamo Trio Eletro",
+        video: "http://www.youtube.com/watch?v=3CKha0SNsoE",
+        lyrics:
+`Vamo vamo trio eletro vamo a ganhar
+Maaais essa gincana trio eu vou te apoiar
+Eeee por toda a minha vida eu vou te amar
+Caaada vez te quero mais`
+      },
+      {
+        title: "Manto Sagrado",
+        video: "http://www.youtube.com/watch?v=8-gOFop_O9M",
+        lyrics:
+`E, deste curso não esqueço
+Todas vezes que vencemos
+Sei porque de tanto te amar
+Eu, faço tudo pra te ver vencer
+Mas pode ganhar ou perder
+O sentimento não vai acabar!
+E vamo vamo Eletro
+Eu só quero honrar
+Esse manto sagrado, vamos à ganhar!`
+      },
+      {
+        title: "Força Eletro",
+        video: "http://www.youtube.com/watch?v=7bT8wtNiCfA",
+        lyrics:
+`Fooorça Eletro (Força Eletro)
+Muita cachaça! (Muita cachaça)
+Força Eletro, campeão somos nós
+(Força Eletro, campeão somos nós)
+
+Ole ole, Ole oleee
+Força Eletro, Ole ole (x2)
+Força Eletro Ole ole ole
+Ole ole ole, Ole ole ole (x2)`
+      },
+      {
+        title: "Hino da Eletro - Olodum",
+        video: "http://www.youtube.com/watch?v=VODy47zgmYQ",
+        lyrics:
+`Ô Ô Ô eôôô
+Lêlêlêlêlêlêlê
+Ô Ô Ô eôôô
+Lêlêlêlelele
+Ô Ô Ô eôôô
+O Trio Eletro chegoou`
+      },
+      {
+        title: "Sangue Azul",
+        lyrics:
+`Trio eletro é a maior do sul
+Onde for eu sempre estarei
+Nas minhas veias corre o sangue azul
+Eu nunca te abandonarei
+Dale ô dale eletro oleêeêe
+Hoje nós temos que ganhar
+Dale ô dale eletro oleêeêe
+Hoje nós vamos festejar`
+      },
+      {
+        title: "Não quero dinheiro",
+        original: "Não quero dinheiro (Tim Maia)",
+        lyrics:
+`Não quero dinheiro
+Vou pedir pra você jogar
+Vou pedir pra você cantar
+Trio Eletro
+Eu te quero bem
+Vou pedir pra você gostar
+Vou pedir pra você me amar
+Trio eletro, meu amor
+A semana inteira
+Fiquei esperando
+Pra te ver sorrindo
+Pra te ver jogando
+Quando a gente ama
+Não pensa em dinheiro
+Só se quer amar
+Se quer amar
+Se quer amar
+De jeito maneira
+Não quero dinheiro
+Quero amor sincero
+Isto é que eu espero
+Grito ao mundo inteiro
+O trio é guerreiro e veio pra ganhar`
+      },
+      {
+        title: "La festa",
+        video: "http://www.youtube.com/watch?v=VODy47zgmYQ",
+        lyrics:
+`Foi numa festa la na Liberato
+É lá que rola festa sim senhor
+Rapaziada puro sentimento
+A que mais canta pela Eletro
+Senti na pele aquela energia
+Quando cheguei na frente do saguão
+O trio eletro todo reunido
+Tremia o resto da fundação
+Começou a gincana, agora vou mostrar pro que vim
+Então saiam da frente
+Por ele eu vou até o fim
+E dale dale dale Eletro!
+E dale dale dale Eletro!
+Tu vais vencer, sou hepta e ninguém mais.`
       },
     ],
   },
@@ -444,7 +711,8 @@ um chuchu`
     },
     data: [
       {
-        title: "Whisky a go-go",
+        title: "Whisky a Go Go",
+        original: "Whisky a Go Go (Roupa Nova)",
         lyrics:
 `Foi na gincana que eu te conheci
 TNT tu és o meu amor
@@ -465,6 +733,7 @@ E vou até o finaal`
       },
       {
         title: "O sentimento não para",
+        original: "Anna (Los Hermanos)",
         lyrics:
 `Sempre ao teu lado até o fim
 Minha vida é você 
@@ -499,6 +768,7 @@ Dale dale TNT`
       },
       {
         title: "Vamos ser",
+        original: "Pingos de amor (Kid Abelha)",
         lyrics: 
 `Vamos ser 
 Outra vez campeões 
@@ -524,6 +794,7 @@ Pelo nosso amor`
       },
       {
         title: "Vermelho é a cor da Fundação",
+        original: "Querência Amada",
         lyrics: 
 `Quem quiser saber quem sou 
 Olha para o meu saguão 
@@ -546,6 +817,7 @@ Eu quero ser campeão!`
       },
       {
         title: "Equipe com mais gatas",
+        original: "Trair a Minha Namorada (MC Jamaica, MC Ricardo)",
         lyrics: 
 `Hoje toca a bateria 
 Da equipe com mais gatas 
@@ -579,6 +851,7 @@ Dale ô, dale ô, dale ô, dale ô`
       },
       {
         title: "Muito mais que um vício",
+        original: "Seven Nation Army (The White Stripes)",
         lyrics: 
 `Muito mais que um vício 
 Muito mais que paixão 
@@ -603,6 +876,7 @@ E dale TNT, e dale TNT`
       },
       {
         title: "Olha a explosão",
+        original: "Olha a Explosão (MC Kevinho)",
         lyrics: 
 `Essa equipe é terrorista 
 É especialista 
