294843d00d51d65d8f6fc304a68bd8f2206a2883
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2018-03-29 02:13:06 -0300

    Preparar para release

--- data/Songs.js
+++ data/Songs.js
@@ -163,7 +163,7 @@ E dalhe dalhe TRÔ
         title: "Amor descontrolado",
         video: "https://www.youtube.com/watch?v=BWgDJ_WAwiw",
         lyrics:
-`sse amor descontrolado
+`Esse amor descontrolado
 Nunca vou deixar de lado
 Vamos juntos minha Trô
 Eu te sigo aonde for
@@ -474,6 +474,244 @@ Desde o início eu lutei por ti.`
   },
   {
     meta: {
+      displayName: "TNT",
+      color: '#d63031',
+    },
+    data: [
+      {
+        title: "Whisky a Go Go",
+        original: "Whisky a Go Go (Roupa Nova)",
+        lyrics:
+`Foi na gincana que eu te conheci
+TNT tu és o meu amor
+E é por isso que hoje estou aqui
+Tenho o vermelho como a minha cor
+Senti na pele a tua energia
+Batendo forte no meu coração
+O dia todo passa num segundo
+O tempo voa mais do que a canção
+Quase no fim do dia
+Uma alegria você me deu
+Agita a bateria
+Que agora o Libas é meu
+E dale, dale, dale, TNT! 
+E dale, dale, dale, TNT!  
+Contigo estou, 
+E vou até o finaal`
+      },
+      {
+        title: "O sentimento não para",
+        original: "Anna (Los Hermanos)",
+        lyrics:
+`Sempre ao teu lado até o fim
+Minha vida é você 
+E a equipe TNT 
+Sempre tão linda 
+Juntos nós iremos te apoiar 
+E a taça levantar 
+Na alegria e na dor 
+O sentimento não para 
+Pois toda essa família 
+Tem amor infinito 
+Cantarei de coração 
+E dale fúria 
+E dale fúria, fúria, fúria!  
+E daledale ô`
+      },
+      {
+        title: "Fúria, estou contigo",
+        lyrics: 
+`Fúria, estou contigo 
+E prometo pra sempre te apoiar 
+Não importa o que aconteça 
+Na fundação, eu quero festejar 
+Fúria vermelha 
+Explode em alegria
+Dale TNT 
+Dale dale TNT 
+Família querida
+Jamais será esquecida
+Dale TNT
+Dale dale TNT`
+      },
+      {
+        title: "Vamos ser",
+        original: "Pingos de amor (Kid Abelha)",
+        lyrics: 
+`Vamos ser 
+Outra vez campeões 
+TNT 
+Sempre meu amor
+Shalaialaialaialaialaialaialaiaaaaa, TNT (2x)`
+      },
+      {
+        title: "Oh Fúria",
+        lyrics: 
+`Levamos
+A nossa paixão 
+Na camiseta 
+Pela Fundação 
+Oh fúria 
+Vou te acompanhar 
+Em qualquer momento 
+Em qualquer lugar 
+E daledaledaledale fúria 
+E daledale ô 
+Pela família 
+Pelo nosso amor`
+      },
+      {
+        title: "Vermelho é a cor da Fundação",
+        original: "Querência Amada",
+        lyrics: 
+`Quem quiser saber quem sou 
+Olha para o meu saguão 
+E grita junto comigo 
+Vermelho é a cor da Fundação! 
+A bomba identifica 
+Qual a minha procedência 
+Somos do curso mais velho 
+Ganhador por excelência! 
+TNT, querida e brava
+Te dou meu sangue, jogo com raça 
+TNT, meu coração 
+A vitória é vermelha, já não sai mais da cabeça 
+Eu quero ser campeão! 
+TNT, querida e brava 
+Te dou meu sangue, jogo com raça 
+TNT, meu coração 
+A vitória é vermelha, já não sai mais da cabeça 
+Eu quero ser campeão!`
+      },
+      {
+        title: "Equipe com mais gatas",
+        original: "Trair a Minha Namorada (MC Jamaica, MC Ricardo)",
+        lyrics: 
+`Hoje toca a bateria 
+Da equipe com mais gatas 
+A fúria vai sacudir 
+Toda gurizada 
+E na gincana eu vou 
+Levantar de novo a taça 
+E na gincana eu vou
+Levantar de novo a taça`
+      },
+      {
+        title: "Somos a superação",
+        lyrics: 
+`Cinco estrelas 
+Um só coração 
+Família TNT 
+Somos a superação 
+Podemos perder 
+Mas nem quero saber 
+Das cinzas renascemos 
+Pra fazer o Libas tremer`
+      },
+      {
+        title: "TNT é alegria",
+        lyrics: 
+`TNT, é alegria 
+Canta mais alto, que qualquer torcida 
+TNT, é união 
+É a fúria vermelha aqui da Fundação 
+Dale ô, dale ô, dale ô, dale ô`
+      },
+      {
+        title: "Muito mais que um vício",
+        original: "Seven Nation Army (The White Stripes)",
+        lyrics: 
+`Muito mais que um vício 
+Muito mais que paixão 
+Vamos TNT 
+Quero ser campeão`
+      },
+      {
+        title: "Voltamos das cinzas",
+        lyrics: 
+`Voltamos das cinzas 
+Para ser campeão 
+A cada gincana
+A fúria honra
+A tradição
+
+Voamos bem alto
+Mas com os pés no chão
+Em busca do hexa da família rubra da fundação
+
+E dale TNT, e dale TNT
+E dale TNT, e dale TNT`
+      },
+      {
+        title: "Olha a explosão",
+        original: "Olha a Explosão (MC Kevinho)",
+        lyrics: 
+`Essa equipe é terrorista 
+É especialista 
+Olha o que ela faz no movimento com as amigas 
+Essa equipe é terrorista 
+É especialista 
+Olha o que ela faz no movimento com as amigas 
+Olha o que ela faz no movimento com as amigas 
+É muito explosiva não mexe com ela não 
+É muito explosiva não manda ofício não 
+Olha a explosão 
+Quando a rua achar o vilão 
+Quando a taça entrar no saguão 
+Quando invadirmos essa fundação 
+Ão ão ão ão`
+      },
+      {
+        title: "Até a morte",
+        original: "Mulher de Fases (Raimundos)",
+        lyrics: 
+`TNT
+A hora é agora
+Vamos mostrar quem é que manda aqui na Fundação
+Lembro de todas as nossas glórias
+Conquistadas com bravura e com o coração
+Essa torcida que não para de cantar
+Mesmo se não ganhar
+TNT nós te amamos 
+E tu sempre vai ter 
+Este curso te apoiando, até a morte`
+      },
+      {
+        title: "Tutá Tutá",
+        lyrics: 
+`Nós temos a bravura 
+Temos a garra e muita raça 
+E tudo que nós queremos 
+É ver a fúria ganhar a taça 
+Vamos matar charada 
+Ganhar esportiva e achar vilão 
+Para no fim da tarde
+Poder gritar é campeão 
+E dale TNT 
+TNT TNT 
+E dale TNT TNT TNT!`
+      },
+      {
+        title: "Eu vou copar",
+        lyrics: 
+`Eu vou copar, pra sempre eu vou copar 
+TNT com raça vai vencer 
+Eu vou cantar, por ti, eu vou cantar 
+TNT pra sempre, até morrer 
+AHHHH 
+TNT equipe do coração 
+Quero ser campeão 
+E na gincana da fundação 
+Te quero ver ganhar 
+Nada muda o pensamento 
+De estar contigo nesse momento 
+E no peito esse sentimento 
+Nunca acabará`
+      },
+    ],
+  },
+  {
+    meta: {
       displayName: "Trio Eletro",
       color: '#0984e3',
     },
@@ -723,244 +961,6 @@ Tu vais vencer, sou hepta e ninguém mais.`
       },
     ],
   },
-  {
-    meta: {
-      displayName: "TNT",
-      color: '#d63031',
-    },
-    data: [
-      {
-        title: "Whisky a Go Go",
-        original: "Whisky a Go Go (Roupa Nova)",
-        lyrics:
-`Foi na gincana que eu te conheci
-TNT tu és o meu amor
-E é por isso que hoje estou aqui
-Tenho o vermelho como a minha cor
-Senti na pele a tua energia
-Batendo forte no meu coração
-O dia todo passa num segundo
-O tempo voa mais do que a canção
-Quase no fim do dia
-Uma alegria você me deu
-Agita a bateria
-Que agora o Libas é meu
-E dale, dale, dale, TNT! 
-E dale, dale, dale, TNT!  
-Contigo estou, 
-E vou até o finaal`
-      },
-      {
-        title: "O sentimento não para",
-        original: "Anna (Los Hermanos)",
-        lyrics:
-`Sempre ao teu lado até o fim
-Minha vida é você 
-E a equipe TNT 
-Sempre tão linda 
-Juntos nós iremos te apoiar 
-E a taça levantar 
-Na alegria e na dor 
-O sentimento não para 
-Pois toda essa família 
-Tem amor infinito 
-Cantarei de coração 
-E dale fúria 
-E dale fúria, fúria, fúria!  
-E daledale ô`
-      },
-      {
-        title: "Fúria, estou contigo",
-        lyrics: 
-`Fúria, estou contigo 
-E prometo pra sempre te apoiar 
-Não importa o que aconteça 
-Na fundação, eu quero festejar 
-Fúria vermelha 
-Explode em alegria
-Dale TNT 
-Dale dale TNT 
-Família querida
-Jamais será esquecida
-Dale TNT
-Dale dale TNT`
-      },
-      {
-        title: "Vamos ser",
-        original: "Pingos de amor (Kid Abelha)",
-        lyrics: 
-`Vamos ser 
-Outra vez campeões 
-TNT 
-Sempre meu amor
-Shalaialaialaialaialaialaialaiaaaaa, TNT (2x)`
-      },
-      {
-        title: "Oh Fúria",
-        lyrics: 
-`Levamos
-A nossa paixão 
-Na camiseta 
-Pela Fundação 
-Oh fúria 
-Vou te acompanhar 
-Em qualquer momento 
-Em qualquer lugar 
-E daledaledaledale fúria 
-E daledale ô 
-Pela família 
-Pelo nosso amor`
-      },
-      {
-        title: "Vermelho é a cor da Fundação",
-        original: "Querência Amada",
-        lyrics: 
-`Quem quiser saber quem sou 
-Olha para o meu saguão 
-E grita junto comigo 
-Vermelho é a cor da Fundação! 
-A bomba identifica 
-Qual a minha procedência 
-Somos do curso mais velho 
-Ganhador por excelência! 
-TNT, querida e brava
-Te dou meu sangue, jogo com raça 
-TNT, meu coração 
-A vitória é vermelha, já não sai mais da cabeça 
-Eu quero ser campeão! 
-TNT, querida e brava 
-Te dou meu sangue, jogo com raça 
-TNT, meu coração 
-A vitória é vermelha, já não sai mais da cabeça 
-Eu quero ser campeão!`
-      },
-      {
-        title: "Equipe com mais gatas",
-        original: "Trair a Minha Namorada (MC Jamaica, MC Ricardo)",
-        lyrics: 
-`Hoje toca a bateria 
-Da equipe com mais gatas 
-A fúria vai sacudir 
-Toda gurizada 
-E na gincana eu vou 
-Levantar de novo a taça 
-E na gincana eu vou
-Levantar de novo a taça`
-      },
-      {
-        title: "Somos a superação",
-        lyrics: 
-`Cinco estrelas 
-Um só coração 
-Família TNT 
-Somos a superação 
-Podemos perder 
-Mas nem quero saber 
-Das cinzas renascemos 
-Pra fazer o Libas tremer`
-      },
-      {
-        title: "TNT é alegria",
-        lyrics: 
-`TNT, é alegria 
-Canta mais alto, que qualquer torcida 
-TNT, é união 
-É a fúria vermelha aqui da Fundação 
-Dale ô, dale ô, dale ô, dale ô`
-      },
-      {
-        title: "Muito mais que um vício",
-        original: "Seven Nation Army (The White Stripes)",
-        lyrics: 
-`Muito mais que um vício 
-Muito mais que paixão 
-Vamos TNT 
-Quero ser campeão`
-      },
-      {
-        title: "Voltamos das cinzas",
-        lyrics: 
-`Voltamos das cinzas 
-Para ser campeão 
-A cada gincana
-A fúria honra
-A tradição
-
-Voamos bem alto
-Mas com os pés no chão
-Em busca do hexa da família rubra da fundação
-
-E dale TNT, e dale TNT
-E dale TNT, e dale TNT`
-      },
-      {
-        title: "Olha a explosão",
-        original: "Olha a Explosão (MC Kevinho)",
-        lyrics: 
-`Essa equipe é terrorista 
-É especialista 
-Olha o que ela faz no movimento com as amigas 
-Essa equipe é terrorista 
-É especialista 
-Olha o que ela faz no movimento com as amigas 
-Olha o que ela faz no movimento com as amigas 
-É muito explosiva não mexe com ela não 
-É muito explosiva não manda ofício não 
-Olha a explosão 
-Quando a rua achar o vilão 
-Quando a taça entrar no saguão 
-Quando invadirmos essa fundação 
-Ão ão ão ão`
-      },
-      {
-        title: "Até a morte",
-        original: "Mulher de Fases (Raimundos)",
-        lyrics: 
-`TNT
-A hora é agora
-Vamos mostrar quem é que manda aqui na Fundação
-Lembro de todas as nossas glórias
-Conquistadas com bravura e com o coração
-Essa torcida que não para de cantar
-Mesmo se não ganhar
-TNT nós te amamos 
-E tu sempre vai ter 
-Este curso te apoiando, até a morte`
-      },
-      {
-        title: "Tutá Tutá",
-        lyrics: 
-`Nós temos a bravura 
-Temos a garra e muita raça 
-E tudo que nós queremos 
-É ver a fúria ganhar a taça 
-Vamos matar charada 
-Ganhar esportiva e achar vilão 
-Para no fim da tarde
-Poder gritar é campeão 
-E dale TNT 
-TNT TNT 
-E dale TNT TNT TNT!`
-      },
-      {
-        title: "Eu vou copar",
-        lyrics: 
-`Eu vou copar, pra sempre eu vou copar 
-TNT com raça vai vencer 
-Eu vou cantar, por ti, eu vou cantar 
-TNT pra sempre, até morrer 
-AHHHH 
-TNT equipe do coração 
-Quero ser campeão 
-E na gincana da fundação 
-Te quero ver ganhar 
-Nada muda o pensamento 
-De estar contigo nesse momento 
-E no peito esse sentimento 
-Nunca acabará`
-      },
-    ],
-  }
 ]
 
 export default Songs;
