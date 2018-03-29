4f3549cdc0b3324f5d935e61e5b61d36887e198d
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2018-03-28 21:23:07 -0300

    Adicionar visualização de original e link pro video

--- ui/MusicDisplay.js
+++ ui/MusicDisplay.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Linking, TouchableOpacity } from 'react-native';
 
 export default class MusicDisplay extends React.Component {
+  handleOpenVideo() {
+    Linking.openURL(this.props.music.video).catch(err => console.error('An error occurred', err));
+  }
   render() {
 
     return (
-      <View style={styles.container}>
-        <Text style={styles.lyrics}> { this.props.music.lyrics }</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        { this.props.music.original ? <Text style={styles.original}>{ this.props.music.original }</Text> : null}
+          <Text style={styles.lyrics}> { this.props.music.lyrics }</Text>
+          { this.props.music.video ? 
+            <TouchableOpacity onPress={this.handleOpenVideo.bind(this)}>
+              <Text style={ styles.video }>Ver no YouTube</Text>
+            </TouchableOpacity>
+            : null
+          }
+      </ScrollView>
     );
   }
 }
@@ -15,10 +25,27 @@ export default class MusicDisplay extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
   },
   lyrics: {
     fontSize: 20,
     lineHeight: 30,
+    textAlign: 'center',
+    marginTop: 30,
   },
+  original: {
+    textAlign: 'center',
+    color: "rgba(0, 0, 0, 0.5)",
+    marginTop: 10,
+  },
+  video: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: "#F5F5F5",
+    backgroundColor: "rgba(255, 0, 0, 0.75)",
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 20,
+    marginBottom: 20,
+    padding: 10,
+  }
 });
